c11d50571c0dca24cdbde338ab7cdddc6582e2f9
nameinan <nandailogic@gmail.com>
2017-07-25 13:38:00 +0530

    Add authentication factory and controller

--- public/app/services/authService.js
+++ public/app/services/authService.js
@@ -32,9 +32,49 @@ angular.module('authService',[])
 			return $http.get('/api/me')
 		}
 		else{
-			$q.reject({message:'Use has no token'})
+			$q.reject({message:'Use has no token'});
 		}
 	}
+	
+	return authFactory;
+})
 
 
+
+.factory('AuthToken',function($window){
+	const authFactory = {};
+	authFactory.getToken= function(){
+		return $window.localStorage.getItem('token');
+	}
+
+	authFactory.setToken = function(token){
+        if (token) {
+        	$window.localStorage.setItem('token',token);
+        }else{
+        	$window.localStorage.removeItem('token');
+        }
+	}
+
+   return authFactory;
+});
+
+
+
+
+.factory('AuthInterceptor', function($q,$location,AuthToken){
+	const interceptorFactory = {};
+	interceptorFactory.request=function(config){
+		var token = AuthToken.getToken();
+		if (token) {
+			config.header('x-access-token')=token;
+		}
+		return config;
+	}
+	interceptorFactory.responseError= function(response){
+		if (response.status==403) {
+			$location.path('/login');
+			return $q.reject(response);
+		}
+	}
+	return interceptorFactory;
 });
